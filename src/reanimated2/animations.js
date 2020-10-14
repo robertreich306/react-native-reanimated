5634205cd7cffce6966402158af2c1c07d4729f3
karol-bisztyga <karol.bisztyga@swmansion.com>
2020-10-14 08:32:59 +0200

    fix no callback

--- src/reanimated2/animations.js
+++ src/reanimated2/animations.js
@@ -42,7 +42,7 @@ export function cancelAnimation(sharedValue) {
 }
 
 function callbackDecorator(callback) {
-  if (callback.__worklet) {
+  if (!callback || callback.__worklet) {
     return callback;
   }
   return (isFinished) => {
